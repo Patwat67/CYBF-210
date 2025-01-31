ebe250ad96a34a8ab016db7bbe1feb5818415f3e
Patwat67 <48216540+Patwat67@users.noreply.github.com>
2025-01-30 20:10:51 -0500

    started work on create_table method and SQL sanatizing

--- to-do.py
+++ to-do.py
@@ -1,4 +1,39 @@
 import sqlite3
+from unittest import case
+
+
+def isValidSQLType(type:str) -> bool:
+    if type in ["TEXT", "REAL", "INTEGER", "BLOB"]:
+        return True
+    else:
+        return False
+
+def isValidSQLModifier(modifier:str) -> bool:
+    match modifier:
+        case "NOT NULL":
+            return True
+        case str() as modifier if modifier.startswith("DEFAULT "):
+            modifier.replace("DEFAULT ", "")
+            if modifier.isdigit():
+                return True
+            else:
+                return False
+        case "PRIMARY KEY":
+            return True
+        case "UNIQUE":
+            return True
+        case "AUTOINCREMENT":
+            return True
+        case str() as modifier if modifier.startswith("CHECK("):
+            modifier.replace("CHECK(", "")
+            if modifier.endswith(")"):
+                return True
+            else:
+                return False
+        case str() as modifier if modifier.startswith("REFERENCE "):
+            modifier.replace("REFERENCE ", "")
+
+
 
 class Database:
     def __init__(self, db_name:str):
@@ -12,8 +47,35 @@ class Database:
     def __del__(self):
         self.connection.close()
 
-    def create_table(self, ):
-        print('hi babe')
+    def close(self):
+        self.connection.close()
+
+    def create_table(self, table_name:str, column_def:tuple[dict[str, str], ...], existence_check:bool=False):
+        query = f"CREATE TABLE IF NOT EXISTS {table_name} ("
+        columns = []
+        if table_name.isidentifier():
+            raise ValueError("Invalid table name")
+        try:
+            for i, d in enumerate(column_def):
+                column_count = len(column_def)
+                column_name = d.get('column_name')
+                column_type = d.get('type')
+                column_constraints = d.get('constraints')
+                if not column_name.isidentifier():
+                    raise ValueError("Invalid column name")
+
+                if not column_constraints:
+                    column_constraints = ''
+
+                if i < column_count - 1:
+                    column = f"{column_name} {column_type} {column_constraints},"
+                else:
+                    column = f"{column_name} {column_type} {column_constraints});"
+                columns.append(column)
+            self.cursor.execute(query + " ".join(columns))
+            self.connection.commit()
+        except Exception as e:
+            print(e)
 
     def fetch(self, query:str, num_rows:int):
         self.cursor.execute(query)
@@ -26,6 +88,15 @@ class Database:
 
     def commit(self):
         self.connection.commit()
+
+test = Database("test.db")
+test.create_table("test", (
+    {"name": "id", "type": "INTEGER"},
+    {"name": "name", "type": "TEXT", "constraints": "NOT NULL"},
+    {"name": "waga", "type": "TEXT", "constraints": "NOT NULL"},
+    )
+)
+test.close()
 
 # #Full List of Parameters (Categorized)
 # 1. Data Types
